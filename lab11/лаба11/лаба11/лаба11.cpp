71a3fc85f0030654108086c68de44d4be298d428
allmeste <90242419+allmeste@users.noreply.github.com>
2023-02-06 08:22:19 +0300

    Add files via upload

---
+++ lab11/лаба11/лаба11/лаба11.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+float f(float x, float n);
+int fact(float a);
+
+void main()
+{
+	float x, n;
+	float b,c;
+	setlocale(LC_CTYPE, "Russian");
+	cout << "Введите х" << endl;
+	cin >> x;
+	cout << "Введите n" << endl;
+	cin >> n;
+	c = f(x, n);
+	b = pow(x, n)/fact(c);
+	/*cin >> b;*/
+	
+	
+	cout << "Значение выражения  x^n/(n-1)!=" << b;
+}
+
+float f(float x, float n)
+{
+	float c;
+	if (n == 0) 
+	{
+		return 1;
+		
+	}
+	else if (n == 1)
+	{
+		return x;
+	}
+	else
+	{
+		c = x * x / n / (n - 1)*f(x,n-2) ;
+		return c;
+	}
+}
+
+int fact(float a)
+{
+	if (a > 1)
+
+		return  a * fact(a - 1);
+		
+	else 
+		return 1;
+	
+}
+	
+
+
---
+++ lab11/лаба11б/лаба11б/лаба11б.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+float f(float x, float n);
+
+
+void main()
+{
+	float m, n;
+	float c;
+	setlocale(LC_CTYPE, "Russian");
+	cout << "Введите m" << endl;
+	cin >> m;
+	cout << "Введите n" << endl;
+	cin >> n;
+	if (n >= 0)
+		c = f(m, n);
+	else
+		cout << "n является отрицательным";
+	cout << "Значение функции " << c;
+}
+
+float f(float x, float n)
+{
+	float c;
+	if (n == 0)
+	{
+		return 1;
+
+	}
+	else if (n < 1)
+	{
+		return -1;
+	}
+	else
+	{
+		c = 2*f(n-1,x);
+		return c;
+	}
+}
+
+
+
+
+
